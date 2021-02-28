0b2cd8ec40fae4026191a874d073bb929f354418
AyushiPar <71605245+AyushiPar@users.noreply.github.com>
2021-02-28 12:07:19 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -24,21 +24,27 @@ function setup() {
   }
 
   for(var j = 40; j <=width; j=j+50){
-    plinkos.push(new Plinko(j,75));
+    plinkos.push(new Plinko(j,55,10));
   }
 
   for(var j = 15; j <=width-10; j=j+50){
-    plinkos.push(new Plinko(j,175));
+    plinkos.push(new Plinko(j,155,10));
   }
 
-  if(frameCount%10===0){
-    particles.push(new Particle(random(width/2-10, width/2+10),10,10));
+  for(var j = 40; j <=width; j=j+50){
+    plinkos.push(new Plinko(j,255,10));
   }
+
+
 }
 
 function draw() {
   background("lightBlue");
   Engine.update(engine);
+
+  if(frameCount%90===0){
+    particles.push(new Particle(random(width/2-10, width/2+10),10,10));
+  }
 
   for(var k = 0; k < divisions.length; k++){
     divisions[k].display();
@@ -52,4 +58,5 @@ function draw() {
     plinkos[r].display();
   }
   ground.display();
+
 }
